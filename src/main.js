f727291fc6d192d0b02cf2a12db7aa5393c325d9
kaplya-sys <strim.sv2010@gmail.com>
2023-11-14 14:29:36 +0500

    added info view

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import EventPresenter from './presenter/event-presenter';
 
-const filtersElement = document.querySelector('.trip-controls__filters');
+const tripMainElement = document.querySelector('.trip-main');
+const filtersElement = tripMainElement.querySelector('.trip-controls__filters');
 const eventsElement = document.querySelector('.trip-events');
 
 const eventPresenter = new EventPresenter(
   {
+    tripMainContainer: tripMainElement,
     filterContainer: filtersElement,
     eventsContainer: eventsElement
   }
--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -1,8 +1,9 @@
-import {render} from '../render';
+import {RenderPosition, render} from '../render';
 import EventCardView from '../view/event-card-view';
 import EventEditView from '../view/event-edit-view';
 import EventListView from '../view/event-list-view';
 import FilterListView from '../view/filter-list-view';
+import InfoView from '../view/info-view';
 import SortListView from '../view/sort-list-view';
 
 const EVENT_CARD_COUNT = 3;
@@ -10,12 +11,20 @@ const EVENT_CARD_COUNT = 3;
 export default class EventPresenter {
   eventListComponent = new EventListView();
 
-  constructor ({filterContainer, eventsContainer}) {
+  constructor (
+    {
+      tripMainContainer,
+      filterContainer,
+      eventsContainer
+    }
+  ) {
+    this.tripMainContainer = tripMainContainer;
     this.filterContainer = filterContainer;
     this.eventsContainer = eventsContainer;
   }
 
   init () {
+    render(new InfoView(), this.tripMainContainer, RenderPosition.AFTERBEGIN);
     render(new FilterListView(), this.filterContainer);
     render(new SortListView(), this.eventsContainer);
     render(this.eventListComponent, this.eventsContainer);
