baaa962962925ff238227362092783c6b410f21c
kaplya-sys <strim.sv2010@gmail.com>
2023-11-17 20:07:37 +0500

    added model for destinations, offers and points

---
+++ src/model/offers-model.js
@@ -0,0 +1,18 @@
+import {offers} from '../mocks/offers.js';
+
+export default class OffersModel {
+  getOffers () {
+    this.offers = offers;
+    return offers;
+  }
+
+  getOffersByType (type) {
+    return this.offers.find((offer) => offer.type === type);
+  }
+
+  getOfferItemsById (type, itemsId) {
+    const offer = this.getDataByType(type);
+
+    return offer.offers.filter((item) => itemsId.find((id) => item.id === id));
+  }
+}
---
+++ src/model/points-model.js
@@ -0,0 +1,7 @@
+import {points} from '../mocks/points.js';
+
+export default class PointsModel {
+  getPoints () {
+    return points;
+  }
+}
